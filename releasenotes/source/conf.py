f6bfaef2ae80aea623d5392b72739770a04be4b8
Rajiv Kumar <rajiv.kumar@nectechnologies.in>
2017-01-19 10:42:01 +0530

    Enable release notes translation
    Releasenote translation publishing is being prepared. 'locale_dirs'
    needs to be defined in conf.py to generate translated version of the
    release notes.
    
    Note that this repository might not get translated release notes - or
    no translations at all - but we add the entry here nevertheless to
    prepare for it.
    
    Change-Id: I008e35500dc5c5cc3e9a5d27a418b6dd423948c1

---
+++ releasenotes/source/conf.py
@@ -0,0 +1,13 @@
+# texinfo_appendices = []
+
+# If false, no module index is generated.
+# texinfo_domain_indices = True
+
+# How to display URL addresses: 'footnote', 'no', or 'inline'.
+# texinfo_show_urls = 'footnote'
+
+# If true, do not generate a @detailmenu in the "Top" node's menu.
+# texinfo_no_detailmenu = False
+
+# -- Options for Internationalization output ------------------------------
+locale_dirs = ['locale/']
