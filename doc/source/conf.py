89bfb54ceb08d470e9fd93cb906e7edfbf5f68b9
Kiran_totad <kiran.totad@nectechnologies.in>
2017-06-27 12:36:15 +0530

    Replace deprecated library function os.popen() with subprocess
    os.popen() is deprecated since python 2.6. Resolved with use of
    subprocess module.
    
    Change-Id: I8d838186f854f4938d05133838927598aabc9d3f

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -91,7 +91,9 @@ version = version_info.version_string()
 # These variables are passed to the logabug code via html_context.
 giturl = u'https://git.openstack.org/cgit/openstack/meteos/tree/doc/source'
 git_cmd = "/usr/bin/git log | head -n1 | cut -f2 -d' '"
-gitsha = os.popen(git_cmd).read().strip('\n')
+gitsha = subprocess.Popen(
+    args=git_cmd,
+    shell=True).communicate()[0].strip(`\n`)
 bug_tag = u'docs'
 # source tree
 pwd = os.getcwd()
